7963ba1b9272c59272bed94d66ab11cd5e1152ce
thwe858 <thwethweoo858@gmail.com>
2025-05-13 17:31:38 +0630

    RideZone

---
+++ Online_Shopping/js/promotion.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }  
+        },{
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 1
+                }
+           
+        }]
+    });
+});
